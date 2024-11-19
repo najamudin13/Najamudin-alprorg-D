e127c7773cc5988f7cec0292bba1010d7abdb833
najamudin13 <mudy6166@gmail.com>
2024-11-19 19:54:27 +0800

    Add files via upload

---
+++ prog6.py.py
@@ -0,0 +1,14 @@
+# # maukan pengunaan
+
+# panjang,lebar dan luas
+panjang= 24
+lebar=11
+luas=13
+
+#menghitung luas
+luas= panjang * lebar
+
+# menampilkan hasil
+print("panjang:", panjang)
+print("lebar:", lebar)
+print("luas persegi panjang adalah:", luas=13)
